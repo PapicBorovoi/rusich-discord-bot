d9ca916f10a7b648e538f2c92e635f26568d8833
Yuriy <originalgamerdaddy@icloud.com>
2023-04-22 15:15:23 +0500

    fix.

--- commands/clear.js
+++ commands/clear.js
@@ -6,12 +6,16 @@ module.exports = {
         .setName('clear')
         .setDescription('Clears the queue.'),
     async execute(interaction) {
-        const { queue } = vcInfo[interaction.guildId];
+        try {
+            const { queue } = vcInfo[interaction.guildId];
 
-        if (queue.length === 0) {
+            if (queue.length === 0) {
+                return await interaction.reply('Queue is already empty.');
+            }
+            queue.length = 0;
+            await interaction.reply('Queue cleared.');
+        } catch (err) {
             return await interaction.reply('Queue is already empty.');
         }
-        queue.length = 0;
-        await interaction.reply('Queue cleared.');
     },
 };
--- commands/skip.js
+++ commands/skip.js
@@ -7,13 +7,17 @@ module.exports = {
         .setName('skip')
         .setDescription('Skips the current song.'),
     async execute(interaction) {
-        const { queue, player } = vcInfo[interaction.guildId];
+        try {
+            const { queue, player } = vcInfo[interaction.guildId];
 
-        if (queue.length === 0 && player.state.status === AudioPlayerStatus.Idle) {
+            if (queue.length === 0 && player.state.status === AudioPlayerStatus.Idle) {
+                return await interaction.reply('There is nothing to skip.');
+            } else {
+                player.stop();
+                return await interaction.reply('Skipped the current song.');
+            }
+        } catch (err) {
             return await interaction.reply('There is nothing to skip.');
-        } else {
-            player.stop();
-            return await interaction.reply('Skipped the current song.');
         }
     },
 };
